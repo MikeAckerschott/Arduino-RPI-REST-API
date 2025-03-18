212cfd4713048039113ace5172ce46fc4765251e
MikeAckerschott <mike.ackerschott03@gmail.com>
2025-03-18 15:30:18 +0000

    FreeRTOS 2.7 with pulseview screenshot
    git-svn-id: https://sasscm.han.nl/svn/intoft21_2024_intoft21_p3_11@40 a199be27-03df-3c44-a9bd-3f8a185e7ddc

---
+++ code/FreeRTOS/2.7 en de loop/src/main.cpp
@@ -0,0 +1,64 @@
+// Copied from https://circuitdigest.com/microcontroller-projects/arduino-freertos-tutorial1-creating-freertos-task-to-blink-led-in-arduino-uno
+#include <Arduino_FreeRTOS.h>
+#include <Arduino.h>
+
+const byte LEDPIN1 = 12;
+const byte LEDPIN2 = 11;
+const byte LEDPIN3 = 10;
+const int DELAY1 = 200;
+const int DELAY2 = 300;
+const int DELAYPRINT = 1000;
+
+const unsigned char bufferSize = 250;
+char ptrTaskList[bufferSize];
+
+void TaskBlink1(void *pvParameters);
+void TaskBlink2(void *pvParameters);
+
+void setup()
+{
+  pinMode(LEDPIN3, OUTPUT);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  xTaskCreate(
+      TaskBlink1, "task1", 128, NULL, 1, NULL);
+
+  xTaskCreate(
+      TaskBlink2, "task2", 128, NULL, 1, NULL);
+
+  vTaskStartScheduler();
+}
+
+void loop()
+{
+  while (true)
+  {
+    PORTB ^= (1 << 2);
+  }
+}
+
+void TaskBlink1(void *pvParameters)
+{
+  pinMode(LEDPIN1, OUTPUT);
+  while (1)
+  {
+    Serial.println("Task1");
+    digitalWrite(LEDPIN1, HIGH);
+    vTaskDelay(DELAY1 / portTICK_PERIOD_MS);
+    digitalWrite(LEDPIN1, LOW);
+    vTaskDelay(DELAY1 / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskBlink2(void *pvParameters)
+{
+  pinMode(LEDPIN2, OUTPUT);
+  while (1)
+  {
+    Serial.println("Task2");
+    digitalWrite(LEDPIN2, HIGH);
+    vTaskDelay(DELAY2 / portTICK_PERIOD_MS);
+    digitalWrite(LEDPIN2, LOW);
+    vTaskDelay(DELAY2 / portTICK_PERIOD_MS);
+  }
+}
