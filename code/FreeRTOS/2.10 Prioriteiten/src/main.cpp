0f3ae8b276fa35ddc6098f0b44c93b84ae165f26
MikeAckerschott <mike.ackerschott03@gmail.com>
2025-03-19 11:33:09 +0000

    FreeRTOS done
    git-svn-id: https://sasscm.han.nl/svn/intoft21_2024_intoft21_p3_11@45 a199be27-03df-3c44-a9bd-3f8a185e7ddc

--- code/FreeRTOS/2.10 Prioriteiten/src/main.cpp
+++ code/FreeRTOS/2.10 Prioriteiten/src/main.cpp
@@ -7,38 +7,37 @@ const byte LEDPIN2 = 11;
 const byte LEDPIN3 = 10;
 const int DELAY1 = 200;
 const int DELAY2 = 300;
-const int DELAYPRINT = 1000;
 
 const unsigned char bufferSize = 250;
 char ptrTaskList[bufferSize];
 
 void TaskBlink1(void *pvParameters);
 void TaskBlink2(void *pvParameters);
+void TaskBlink3(void *pvParameters);
 
 void setup()
 {
+  pinMode(LEDPIN1, OUTPUT);
+  pinMode(LEDPIN2, OUTPUT);
   pinMode(LEDPIN3, OUTPUT);
   xTaskCreate(
-      TaskBlink1, "task1", 128, NULL, 1, NULL);
+      TaskBlink1, "task1", 128, NULL, 2, NULL);
 
   xTaskCreate(
-      TaskBlink2, "task2", 128, NULL, 1, NULL);
+      TaskBlink2, "task2", 128, NULL, 2, NULL);
+
+  xTaskCreate(
+      TaskBlink3, "task3", 128, NULL, 1, NULL);
 
   vTaskStartScheduler();
 }
 
 void loop()
 {
-  while (true)
-  {
-    PORTB |= (1 << PORTB2);
-    PORTB &= ~(1 << PORTB2);
-  }
 }
 
 void TaskBlink1(void *pvParameters)
 {
-  pinMode(LEDPIN1, OUTPUT);
   while (1)
   {
     digitalWrite(LEDPIN1, HIGH);
@@ -50,12 +49,21 @@ void TaskBlink1(void *pvParameters)
 
 void TaskBlink2(void *pvParameters)
 {
-  pinMode(LEDPIN2, OUTPUT);
   while (1)
   {
     digitalWrite(LEDPIN2, HIGH);
     delay(DELAY2 / portTICK_PERIOD_MS);
     digitalWrite(LEDPIN2, LOW);
     delay(DELAY2 / portTICK_PERIOD_MS);
+  }
+}
+
+// toggle task based on 2.4
+void TaskBlink3(void *pvParameters)
+{
+  while (1)
+  {
+    PORTB |= (1 << PORTB2);
+    PORTB &= ~(1 << PORTB2);
   }
 }
